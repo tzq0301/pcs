f0d2c3377c0c6376af7cf176ea7e3d57dc5d8799
tzq0301 <1661610563@qq.com>
2022-02-28 13:22:22 +0800

    feat(auth): 增加 User 类与 Sex、Role 枚举类

---
+++ auth/src/main/java/cn/tzq0301/auth/entity/user/Sex.java
@@ -0,0 +1,20 @@
+package cn.tzq0301.auth.entity.user;
+
+/**
+ * @author tzq0301
+ * @version 1.0
+ */
+public enum Sex {
+    FEMALE(0), // 女
+    MALE(1); // 男
+
+    private final Integer sex;
+
+    Sex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+}
