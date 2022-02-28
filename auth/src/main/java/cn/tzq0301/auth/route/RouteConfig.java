6266827fa88a5184d4f91806242f64be6c39833f
tzq0301 <1661610563@qq.com>
2022-02-28 13:21:56 +0800

    test(auth): 增加测试 Handler

--- auth/src/main/java/cn/tzq0301/auth/route/RouteConfig.java
+++ auth/src/main/java/cn/tzq0301/auth/route/RouteConfig.java
@@ -1,12 +1,11 @@
 package cn.tzq0301.auth.route;
 
-import cn.tzq0301.result.Result;
+import cn.tzq0301.auth.api.TestHandler;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 /**
@@ -15,9 +14,18 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
  */
 @SpringBootConfiguration
 public class RouteConfig {
+    private final TestHandler testHandler;
+
+    public RouteConfig(TestHandler testHandler) {
+        this.testHandler = testHandler;
+    }
+
     @Bean
     public RouterFunction<ServerResponse> router() {
         return route()
-                .GET("/test", request -> ServerResponse.ok().contentType(APPLICATION_JSON).bodyValue(Result.success("test"))).build();
+                .GET("/test", testHandler::test)
+                .GET("/student", testHandler::student)
+                .GET("/admin", testHandler::admin)
+                .build();
     }
 }
