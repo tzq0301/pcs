a671f78d72bc14df00c3c9aa9a734e18c690dcdf
tzq0301 <1661610563@qq.com>
2022-02-28 14:12:20 +0800

    feat(test): 测试 User 的插入和基于 UserId 的查找

---
+++ auth/src/test/java/cn/tzq0301/auth/repository/UserRepositoryTest.java
@@ -0,0 +1,46 @@
+package cn.tzq0301.auth.repository;
+
+import cn.tzq0301.auth.entity.user.Role;
+import cn.tzq0301.auth.entity.user.Sex;
+import cn.tzq0301.auth.entity.user.User;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+
+/**
+ * @author tzq0301
+ * @version 1.0
+ */
+@DataMongoTest
+class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @Disabled
+    void testInsertion() {
+        User tzq = new User("2019141460542", "TZQ", "123456", true, Role.STUDENT, Sex.MALE,
+                LocalDate.of(2001, 3, 1), "18968909990", "tzq0301@gmail.com",
+                "330302200103011610", 0);
+
+        userRepository.save(tzq)
+                .map(User::getName)
+                .as(StepVerifier::create)
+                .expectNext("TZQ")
+                .verifyComplete();
+    }
+
+    @Test
+    @Disabled
+    void testFindByUserId() {
+        userRepository.findByUserId("2019141460542")
+                .map(User::getName)
+                .as(StepVerifier::create)
+                .expectNext("TZQ")
+                .verifyComplete();
+    }
+}
