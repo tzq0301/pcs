e05baae63955acecf1570c05c0052daf768414af
tzq0301 <1661610563@qq.com>
2022-03-01 23:35:58 +0800

    feature(auth): 增加 UserResponse 类（用于"使用 userId 获取用户信息"）

---
+++ auth/src/main/java/cn/tzq0301/auth/user/entity/UserResponse.java
@@ -0,0 +1,22 @@
+package cn.tzq0301.auth.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tzq0301
+ * @version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String userId;
+
+    private String password;
+
+    private Boolean enabled;
+
+    private String role;
+}
