d18c923be6c7bec7413fee27401fd27653defd77
peter <2063436123@qq.com>
2022-03-05 09:32:27 +0800

    feat(*): 添加 visit、consult、duty 微服务

---
+++ duty/src/main/java/cn/tzq0301/duty/DutyApplication.java
@@ -0,0 +1,13 @@
+package cn.tzq0301.duty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DutyApplication.class, args);
+    }
+
+}
