f9d3150df9ef70bc6c33fd19b1661e427d14cee0
tzq0301 <1661610563@qq.com>
2022-03-10 17:41:41 +0800

    feat(auth): 为用户查询增加缓存

---
+++ auth/src/main/java/cn/tzq0301/auth/user/manager/UserManager.java
@@ -0,0 +1,42 @@
+package cn.tzq0301.auth.user.manager;
+
+import cn.tzq0301.auth.config.RedisConfig;
+import cn.tzq0301.auth.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author tzq0301
+ * @version 1.0
+ */
+@Service
+@AllArgsConstructor
+@Log4j2
+public class UserManager {
+    private final ReactiveRedisTemplate<String, Object> redisTemplate;
+
+    private static final String USER_HASH_KEY = RedisConfig.USER_NAMESPACE_PREFIX + "user";
+
+    public Mono<User> putUserIdAndUserIdIntoCache(final Mono<User> user) {
+        return user
+                .flatMap(it -> redisTemplate.opsForHash().put(USER_HASH_KEY, it.getUserId(), it))
+                .flatMap(it -> user)
+                .doOnNext(it -> log.info("Put User into Cache -> {}", it));
+    }
+
+    public Mono<User> putUserIdAndUserIdIntoCache(final User user) {
+        return redisTemplate.opsForHash().put(USER_HASH_KEY, user.getUserId(), user)
+                .map(it -> user)
+                .doOnNext(it -> log.info("Put User into Cache -> {}", it));
+    }
+
+    public Mono<User> getUserByUserIdFromCache(final String userId) {
+        return redisTemplate.opsForHash().get(USER_HASH_KEY, userId)
+                .map(obj -> (User) obj)
+                .doOnNext(it -> log.info("Got User from Cache -> {}", it))
+                .onErrorResume(ex -> ex instanceof Exception, Mono::error);
+    }
+}
