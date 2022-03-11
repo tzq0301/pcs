30ce01ed57326983e5f3e6681a5b15ece4574c93
tzq0301 <1661610563@qq.com>
2022-03-11 11:50:50 +0800

    fix(duty): 修复"获取指定初访员/咨询师所有值班"中 day 格式错误（应为 "20220101" 而非 "2022-01-01"）

--- duty/src/main/java/cn/tzq0301/duty/entity/duty/vo/DutyResponse.java
+++ duty/src/main/java/cn/tzq0301/duty/entity/duty/vo/DutyResponse.java
@@ -2,11 +2,14 @@ package cn.tzq0301.duty.entity.duty.vo;
 
 import cn.tzq0301.duty.entity.duty.Pattern;
 import cn.tzq0301.duty.entity.duty.SpecialItem;
+import cn.tzq0301.util.DateUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author tzq0301
@@ -14,9 +17,29 @@ import java.util.List;
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class DutyResponse {
     private List<Pattern> patterns;
 
-    private List<SpecialItem> specials;
+    private List<DutySpecialItem> specials;
+
+    public DutyResponse(List<Pattern> patterns, List<SpecialItem> specials) {
+        this.patterns = patterns;
+        this.specials = specials.stream()
+                .map(item -> new DutySpecialItem(DateUtils.localDateToString(item.getDay()), item.getFrom(),
+                        item.getAddress(), item.getType()))
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class DutySpecialItem {
+        private String day;
+
+        private Integer from;
+
+        private String address;
+
+        private Integer type; // 特殊情况的类型（加班为 0、请假为 1）
+    }
 }
